30545b0e45417a8751529da7b931902b28bc9974
coreyo-git <corey.oconnor@live.com.au>
2023-08-10 09:53:42 +1000

    LoginActivity base setup setup binding UI and Activity setup base credential validation

--- app/src/main/java/com/example/fleetmanagement/LoginActivity.java
+++ app/src/main/java/com/example/fleetmanagement/LoginActivity.java
@@ -3,12 +3,48 @@ package com.example.fleetmanagement;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private EditText emailEditText;
+    private EditText passwordEditText;
+    private Button loginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        setViewIds();
+
+        loginButton.setOnClickListener(view -> {
+            String email = emailEditText.getText().toString();
+            String password = passwordEditText.getText().toString();
+
+            if(isValidCredentials(email, password)) {
+                Toast.makeText(LoginActivity.this, "Login Successful", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(LoginActivity.this, "Invalid Credentials", Toast.LENGTH_SHORT).show();
+                emailEditText.setError("Invalid email");
+                passwordEditText.setError("Invalid password");
+            }
+        });
+    }
+
+    private void setViewIds()
+    {
+        emailEditText = findViewById(R.id.emailEditText);
+        passwordEditText = findViewById(R.id.passwordEditText);
+        loginButton = findViewById(R.id.loginButton);
+    }
+
+    private boolean isValidCredentials(String email, String password)
+    {
+        // Perform validation logic here
+        // Return true if credentials are valid, false otherwise
+        return email.equals("ex@ex.com") &&
+                password.equals("pass123");
     }
 }
