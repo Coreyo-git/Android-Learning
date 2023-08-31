de21f08c7a361cb9e21f887554a69559e36b15bc
coreyo-git <corey.oconnor@live.com.au>
2023-08-31 12:19:06 +1000

    style: Add Delete and Update buttons to VehicleDetails view
    Enhance the appearance and functionality of the VehicleDetails view by
    incorporating "Delete" and "Update" buttons. This change improves the user
    experience by providing clear options for managing vehicle information directly
    from the details page.
    
    - Introduce "Delete" and "Update" buttons to the VehicleDetails layout
    - Adjust layout style to accommodate the new buttons
    - Bind button actions to corresponding event handlers in VehicleDetailsActivity

--- app/src/main/java/com/example/fleetmanagement/VehicleDetailsActivity.java
+++ app/src/main/java/com/example/fleetmanagement/VehicleDetailsActivity.java
@@ -1,18 +1,20 @@
 package com.example.fleetmanagement;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.fleetmanagement.DB.Vehicle;
 import com.example.fleetmanagement.DB.VehicleDao;
 import com.example.fleetmanagement.Utils.MyApp;
-
-import java.util.Random;
 
 public class VehicleDetailsActivity extends AppCompatActivity {
     private TextView vehicleNumber;
@@ -24,6 +26,11 @@ public class VehicleDetailsActivity extends AppCompatActivity {
     private TextView vehicleFuelStatus;
     private TextView vehicleGoodsTemp;
     private Button backButton;
+    private Button updateButton;
+    private Button deleteButton;
+    int vehicleId;
+    Vehicle vehicle;
+    VehicleDao vehicleDao;
 
 
     @Override
@@ -31,13 +38,13 @@ public class VehicleDetailsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_vehicle_details);
         setViewIds();
-        Intent intent = getIntent();
-        VehicleDao vehicleDao = MyApp.getAppDatabase().vehicleDao();
+        vehicleDao = MyApp.getAppDatabase().vehicleDao();
 
         if (getIntent().getIntExtra("vehicleId", -1) != -1){
-            int vehicleId = getIntent().getIntExtra("vehicleId", -1);
+            vehicleId = getIntent().getIntExtra("vehicleId", -1);
 
             vehicleDao.getVehicleById(vehicleId).observe(this, vehicle -> {
+                this.vehicle = vehicle;
                 genVehicleProps(vehicle);
 
                 vehicleNumber.setText(String.valueOf(vehicleId));
@@ -48,6 +55,23 @@ public class VehicleDetailsActivity extends AppCompatActivity {
                 vehicleCurrentLocation.setText(vehicle.getCurrentLocation());
                 vehicleFuelStatus.setText(String.valueOf(vehicle.getFuelStatus()));
                 vehicleGoodsTemp.setText(String.valueOf(vehicle.getGoodsTemperature()));
+
+                updateButton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        if (vehicle != null) {
+                            updateTheVehicle();
+                        }
+                    }
+                });
+                deleteButton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        if (vehicle != null) {
+                            deleteTheVehicle();
+                        }
+                    }
+                });
             });
 
         }else {
@@ -71,6 +95,8 @@ public class VehicleDetailsActivity extends AppCompatActivity {
         vehicleFuelStatus = findViewById(R.id.vehicleFuelStatus);
         vehicleGoodsTemp = findViewById(R.id.vehicleGoodsTemp);
         backButton = findViewById(R.id.backButton);
+        deleteButton = findViewById(R.id.deleteButton);
+        updateButton = findViewById(R.id.updateButton);
     }
 
     public void genVehicleProps(Vehicle vehicle) {
@@ -79,5 +105,49 @@ public class VehicleDetailsActivity extends AppCompatActivity {
         vehicle.setCurrentLocation("Helensvale");
         vehicle.setFuelStatus(55.5);
         vehicle.setGoodsTemperature(12.2);
+    }
+
+    private void updateTheVehicle() {
+        // Creating the view to create the dialog. We are re-using the dialog we created in Week-4 to add new vehicle.
+        View dialogView = getLayoutInflater().inflate(R.layout.dialog_add_vehicle, null);
+        EditText editTextVehicleName = dialogView.findViewById(R.id.editTextVehicleName);
+        EditText editTextVehicleType = dialogView.findViewById(R.id.editTextVehicleType);
+
+        // Pre-set the current vehicle name and type to these edittext views.
+        editTextVehicleName.setText(vehicle.getName());
+        editTextVehicleType.setText(vehicle.getType());
+
+        //Creating the dialog builder to create the pop up dialog.
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setView(dialogView);
+
+        //Setting and implementing the update operation
+        builder.setPositiveButton("Update", (dialog, which) -> {
+            String vehicleName = editTextVehicleName.getText().toString();
+            String vehicleType = editTextVehicleType.getText().toString();
+
+            //update the values
+            vehicle.setName(vehicleName);
+            vehicle.setType(vehicleType);
+            AsyncTask.execute(() -> {
+                vehicleDao.update(vehicle);
+            });
+        });
+
+        // Setting the update cancellation
+        builder.setNegativeButton("Cancel", (dialog, which) -> {
+        // Do nothing or handle any other actions
+            dialog.cancel();
+        });
+        //Creating and showing the dialog.
+        AlertDialog dialog = builder.create();
+        dialog.show();
+
+    }
+    private void deleteTheVehicle() {
+        AsyncTask.execute(() -> {
+            vehicleDao.delete(vehicle);
+            finish();
+        });
     }
 }
