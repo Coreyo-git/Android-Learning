9e881b5be103355bb0cef4c79d22b7349aa39687
coreyo-git <corey.oconnor@live.com.au>
2023-09-07 13:07:56 +1000

    SensorService implements SensorEventListener - added onSensorChanged and onAccuracyChanged methods to suit interface

--- app/src/main/java/com/example/fleetmanagement/SensorUtil/SensorService.java
+++ app/src/main/java/com/example/fleetmanagement/SensorUtil/SensorService.java
@@ -2,9 +2,17 @@ package com.example.fleetmanagement.SensorUtil;
 
 import android.app.Service;
 import android.content.Intent;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.IBinder;
 import androidx.annotation.Nullable;
-public class SensorService extends Service {
+public class SensorService extends Service implements SensorEventListener {
+    private SensorManager sensorManager;
+    private Sensor tempSensor = null; // init null
+    private static final float TEMP_THRESHOLD = 40; // degree celsius
+
     @Override
     public void onCreate() {
         super.onCreate();
@@ -13,6 +21,12 @@ public class SensorService extends Service {
     public int onStartCommand(Intent intent, int flags, int startId)
     {
         return START_STICKY;
+    }
+    @Override
+    public void onSensorChanged(SensorEvent sensorEvent) {
+    }
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
     }
     @Override
     public void onDestroy() {
