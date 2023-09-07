a6fa8b5be6e90f1d2968328bea1c65743551801f
coreyo-git <corey.oconnor@live.com.au>
2023-09-07 13:24:33 +1000

    SensorService setup temp change tracking
    - Handled temp changes in onSensorChanged()
    - Tested temp tracking
    - setup onDestory to handle unregistered event listeners

--- app/src/main/java/com/example/fleetmanagement/SensorUtil/SensorService.java
+++ app/src/main/java/com/example/fleetmanagement/SensorUtil/SensorService.java
@@ -1,12 +1,16 @@
 package com.example.fleetmanagement.SensorUtil;
 
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.IBinder;
+import android.util.Log;
+import android.widget.Toast;
+
 import androidx.annotation.Nullable;
 public class SensorService extends Service implements SensorEventListener {
     private SensorManager sensorManager;
@@ -16,6 +20,21 @@ public class SensorService extends Service implements SensorEventListener {
     @Override
     public void onCreate() {
         super.onCreate();
+        sensorManager = (SensorManager)
+                getSystemService(Context.SENSOR_SERVICE);
+        if (sensorManager.getDefaultSensor(Sensor.TYPE_AMBIENT_TEMPERATURE)
+                != null){
+            tempSensor =
+                    sensorManager.getDefaultSensor(Sensor.TYPE_AMBIENT_TEMPERATURE);
+        }else {
+            Toast.makeText(this, "No Temperature sensor found",
+                    Toast.LENGTH_SHORT).show();
+        }
+
+        if (tempSensor != null) {
+            sensorManager.registerListener(this, tempSensor,
+                    SensorManager.SENSOR_DELAY_NORMAL);
+        }
     }
     @Override
     public int onStartCommand(Intent intent, int flags, int startId)
@@ -24,6 +43,12 @@ public class SensorService extends Service implements SensorEventListener {
     }
     @Override
     public void onSensorChanged(SensorEvent sensorEvent) {
+        if (sensorEvent.sensor.getType() ==
+                Sensor.TYPE_AMBIENT_TEMPERATURE){
+            float tempData = sensorEvent.values[0];
+            Log.d("Sensor data ", "Ambient temperature is: "+tempData+"C");
+        }
+
     }
     @Override
     public void onAccuracyChanged(Sensor sensor, int i) {
@@ -31,6 +56,9 @@ public class SensorService extends Service implements SensorEventListener {
     @Override
     public void onDestroy() {
         super.onDestroy();
+        if (sensorManager != null) {
+            sensorManager.unregisterListener(this);
+        }
     }
     @Nullable
     @Override
